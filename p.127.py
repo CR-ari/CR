b1b3321d3059a5b7ff04568809a3f3b06273dd0b
CR-ari <59080470+CR-ari@users.noreply.github.com>
2020-01-06 17:55:19 +0900

    Create p.127.py

---
+++ p.127.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  6 16:14:48 2020
+
+@author: 9876a
+"""
+
+grid = [['.','.','.','.','.','.'],
+        ['.','0','0','.','.','.'],
+        ['0','0','0','0','.','.'],
+        ['0','0','0','0','0','.'],
+        ['.','0','0','0','0','0'],
+        ['0','0','0','0','0','.'],
+        ['0','0','0','0','.','.'],
+        ['.','0','0','.','.','.'],
+        ['.','.','.','.','.','.']]
+
+line1 = grid[0]
+line2 = grid[1]
+line3 = grid[2]
+line4 = grid[3]
+line5 = grid[4]
+line6 = grid[5]
+line7 = grid[6]
+line8 = grid[7]
+line9 = grid[8]
+
+
+#print(line9[0],line8[0],line7[0],line6[0],line5[0],line4[0],line3[0],line2[0],line1[0])
+
+for i in range(0,8,1):
+    for j in range(1,9,1):
+        print(linej[i])
+
+#for i in range(0,8,1):
+    #for e in range(1,9,-1):
+        #print(linee[i])
+#print(type(line8))
+
