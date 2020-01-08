dcecdc9bdcb429be277d505e6d82975cd1c308b0
CR-ari <59080470+CR-ari@users.noreply.github.com>
2020-01-08 18:37:14 +0900

    Update p.127.py

--- p.127.py
+++ p.127.py
@@ -15,25 +15,23 @@ grid = [['.','.','.','.','.','.'],
         ['.','0','0','.','.','.'],
         ['.','.','.','.','.','.']]
 
-line1 = grid[0]
-line2 = grid[1]
-line3 = grid[2]
-line4 = grid[3]
-line5 = grid[4]
-line6 = grid[5]
-line7 = grid[6]
-line8 = grid[7]
-line9 = grid[8]
+
+grid3 = []
+
+for i in range(0,6,1):
+    for j in range(8,-1,-1):
+        grid2 = grid[j][i]
+        grid3.append(grid2)
+        if i == 6 and j == -1:
+            break
 
 
-#print(line9[0],line8[0],line7[0],line6[0],line5[0],line4[0],line3[0],line2[0],line1[0])
+print(''.join(grid3[0:9]))
+print(''.join(grid3[9:18]))
+print(''.join(grid3[18:27]))
+print(''.join(grid3[27:36]))
+print(''.join(grid3[36:45]))
+print(''.join(grid3[45:54]))
 
-for i in range(0,8,1):
-    for j in range(1,9,1):
-        print(linej[i])
 
-#for i in range(0,8,1):
-    #for e in range(1,9,-1):
-        #print(linee[i])
-#print(type(line8))
 
